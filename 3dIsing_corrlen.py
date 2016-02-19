f60fe941721c7358493115d9f6fb6002f6753501
Peihao Sun <phsun@sherlock-ln01.stanford.edu>
2016-02-18 22:54:24 -0800

    ready for long runs

--- 3dIsing_corrlen.py
+++ 3dIsing_corrlen.py
@@ -1,3 +1,5 @@
+import os
+import pickle
 import numpy as np
 import itertools
 import matplotlib.pyplot as plt
@@ -7,10 +9,19 @@ from metropolis import metropolis_3dI
 size_1d = 32
 size = [size_1d, size_1d, size_1d]
 Ns = np.prod(size) # number of sites
-Kstart = 0.3
-Kend = 0.3
-Ksteps = 1
-K_array = np.linspace(Kstart, Kend, Ksteps, endpoint=False)
+Tstart = 0. # T = 1/K
+Tend = 10. 
+Tsteps = 101
+T_array = np.linspace(Tstart, Tend, Tsteps)
+
+Ninit = 100000
+Ncycles = 1000 
+Nsteps = 100 # steps for each cycle
+
+
+# file header
+header = {'model':'3D Ising', 'size':size, 'T':None, \
+          'Ninit':Ninit, 'Ncycles':Ncycles, 'Nsteps':Nsteps}
 
 # initialize lattice
 lt = np.zeros(size)
@@ -22,11 +33,10 @@ for s in np.nditer(lt, op_flags=['readwrite']):
         s[...] = 1.
 
 # Metropolis algorithm
-Ninit = 100000
-Ncycles = 1000 
-Nsteps = 100 # steps for each cycle
+r_array = np.arange(5)
+for T in T_array:
+    K = 1./T
 
-for K in K_array:
     U = 0 # internal energy, defined as sum of -s_i * s_j
     # initialize by doing Ninit steps
     for i_step in xrange(Ninit):
@@ -34,8 +44,9 @@ for K in K_array:
     #END initialization
     
     # START cycle loop; measure physical quantity after each cycle
-    r_array = np.arange(16)
-    s0sr_array = np.zeros(16)
+    s0sr_array = np.zeros(5)
+    M = []
+    U = []
 
     for i_cycle in xrange(Ncycles):
         for i_step in xrange(Nsteps):
@@ -43,6 +54,8 @@ for K in K_array:
         #END step loop
 
         # measure physical quantities for this cycle
+        M.append(np.sum(lt)*1.)
+        s0 = np.sum(lt)*1. / Ns # average spin, only
         for i_r in xrange(len(r_array)):
             r = r_array[i_r]
             # measure <s0*sr>
@@ -52,18 +65,32 @@ for K in K_array:
                 jpr = (j+r) % size[1]
                 kpr = (k+r) % size[2]
                 s0sr += lt[i,j,k]*(lt[ipr,j,k]+lt[i,jpr,k]+lt[i,j,kpr])
-            s0sr /= Ns * 3. # 3 pairs for each site
+            if r == 1:
+                U.append(-s0sr*1.) # internal energy is defined as sum of
+                                   # s_i*s_j for nn
 
-            s0 = np.sum(lt)*1. / Ns
+            s0sr /= Ns * 3. # 3 pairs for each site
+            
             #print "<s0sr> =", s0sr, "<s0> =", s0
             s0sr_array[i_r] = s0sr_array[i_r] + s0sr - s0**2
             #print "s0sr:", s0sr_array
 
-    print zip(r_array, s0sr_array)
+    #print zip(r_array, s0sr_array)
     s0sr_array = s0sr_array / (Ncycles * 1.)
+    s0 = np.mean(M)*1./Ns
 
-    plt.figure()
-    plt.plot(r_array, -np.log(abs(s0sr_array)))
-    plt.xlabel("r")
-    plt.ylabel(r"$-\ln(<\sigma_0\sigma_r>-<s_0><s_r>)$")
-    plt.savefig("corrlen_K_%.2f.png" % K)
+    #plt.figure()
+    #plt.plot(r_array, -np.log(abs(s0sr_array)))
+    #plt.xlabel("r")
+    #plt.ylabel(r"$-\ln(<\sigma_0\sigma_r>-<s_0><s_r>)$")
+    #plt.savefig("corrlen_K_%.2f.png" % K)
+
+
+    # saving data to file
+    os.chdir("/home/phsun/212project/%d" % size_1d)
+    f = open("T_%.2f.pickle" % T, 'w')
+    header['T'] = T
+    pickle.dump({'header':header, 'M':M, 'M_ave':np.mean(M),\
+                 'U':U, 'U_ave':np.mean(U), 's0':s0, \
+                 'r':r_array, 's0sr':s0sr_array}, f)
+    f.close() 
